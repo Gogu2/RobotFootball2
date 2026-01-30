3e8da9f499458f9b639f8feef5ff6bcd3fa66fc3
Stefan Udrea <stefan.udrea@gmail.com>
2026-01-30 10:42:44 +0200

    version label

--- RobotFootball.service.worker.js
+++ RobotFootball.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1769761846|8456568277';
+const CACHE_VERSION = '1769762476|9086402871';
 /** @type {string} */
 const CACHE_PREFIX = 'RobotFootball-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
