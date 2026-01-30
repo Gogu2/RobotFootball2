a2fab571bc08a148fe47a70af17d128b0e609e5b
Stefan Udrea <stefan.udrea@gmail.com>
2026-01-30 11:12:48 +0200

    R to bring back the ball

--- RobotFootball.service.worker.js
+++ RobotFootball.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1769762476|9086402871';
+const CACHE_VERSION = '1769764293|10902898897';
 /** @type {string} */
 const CACHE_PREFIX = 'RobotFootball-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
